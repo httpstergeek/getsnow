b4ab1a65b614b5ce7c4ece9efe9333ee0742944e
bernie macias <berniem@zillow.com>
2016-03-11 13:45:31 -0800

    add snowincident code prototype

---
+++ bin/snowincident.py
@@ -0,0 +1,209 @@
+# encoding: utf-8
+# Author: Bernardo Macias <bmacias@httpstergeek.com>
+#
+#
+# All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'Bernardo Macias '
+__credits__ = ['Bernardo Macias']
+__license__ = "ASF"
+__version__ = "2.0"
+__maintainer__ = "Bernardo Macias"
+__email__ = 'bmacias@httpstergeek.com'
+__status__ = 'Production'
+
+
+import util
+import time
+import datetime
+import requests
+from logging import INFO
+from splunklib.searchcommands import \
+    dispatch, GeneratingCommand, Configuration, Option
+
+
+logger = util.setup_logger(INFO)
+
+
+def keyreplace(record, keyto, keyfrom, username, password):
+    if 'value' in record[keyto]:
+        value = record[keyto]['value']
+        if value == 'system':
+            record[keyto] = 'system'
+        else:
+            if value in retrived_objects:
+                record[keyto] = retrived_objects[value]
+            else:
+                response = requests.get(record[keyto]['link'],
+                                        auth=(username, password),
+                                        headers={'Accept': 'application/json'}
+                                        )
+                if response.status_code == 200:
+                    usern = response.json()['result'][keyfrom]
+                    record[keyto] = usern
+                    retrived_objects[value] = usern
+    return record
+
+
+def filterbuilder(filter, filterarg):
+    if filterarg:
+        filterarg = filterarg.split(',')
+        filterarg = [filter.strip() + '='+x.strip() for x in filterarg if x]
+        filterarg = '^OR'.join(filterarg).replace(' ', '%20')
+    else:
+        filterarg = ''
+    return filterarg
+
+
+def getRecords(url, auth=None):
+    while url:
+        response = requests.get(url,
+                                auth=(auth['user'], auth['password']),
+                                headers={'Accept': 'application/json'}
+                                )
+        headers = response.headers
+        if 'Link' in headers:
+            links = headers['Link'].split(',')
+            for link in links:
+                if 'rel="next"' in link:
+                    url = link.split(';')[0][1:-1]
+                    break
+                else:
+                    url = None
+        else:
+            url = None
+        if response.status_code == 200:
+            results = response.json()
+            if 'result' in results:
+                for result in results['result']:
+                    yield result
+
+
+@Configuration(local=True)
+class getUserCommand(GeneratingCommand):
+    """ %(synopsis)
+
+    ##Syntax
+    .. code-block::
+    getuser env=<str> user_name=<str> daysAgo=<int> env=<str>
+
+    ##Description
+
+    Returns json events for Service Now API from tables.  Limit 1000 events.
+
+    ##Example
+
+    Return json events where where active is true and contact_type is phone for the past 30 days.
+
+    .. code-block::
+        | getsnow user_name=rick daysAgo=30
+        OR
+        | getsnow env=production user_name=mortey
+
+    """
+
+    user_names = Option(
+            doc='''**Syntax:** **table=***<str>*
+        **Description:** user_name of user''',
+            require=False)
+    assignment_group = Option(
+            doc='''**Syntax:** **table=***<str>*
+        **Description:** assignment_group incident assigned to ticket''',
+            require=False)
+    filterby = Option(
+            doc='''**Syntax:** **table=***<str>*
+        **Description:** assignment_group incident assigned to ticket''',
+            require=False)
+
+    daysAgo = Option(
+            doc='''**Syntax:** **table=***<str>*
+        **Description:** How many days ago to retrieve incidents''',
+            require=False)
+    daysby = Option(
+            doc='''**Syntax:** **table=***<str>*
+        **Description:** How many days ago to retrieve incidents''',
+            require=False)
+
+    env = Option(
+            doc='''**Syntax:** **env=***<str>*
+        **Description:** Environment to query. Environment must be in conf. Default production.''',
+            require=False)
+
+    def generate(self):
+        # Parse and set arguments
+        logger = util.setup_logger(INFO)
+
+        # get config
+        env = self.env.lower() if self.env else 'production'
+        conf = util.getstanza('getsnow', env)
+        proxy_conf = util.getstanza('getsnow', 'global')
+        username = conf['user']
+        password = conf['password']
+        value_replacements = conf['value_replacements']
+        user_names = self.user_names
+        assigment_groups = self.assignment_groups
+        filterby = self.filterby if self.filterby else 'assigned_to'
+        daysby = self.daysby if self.daysby else 'opened_at'
+        url = conf['url']
+        time_range = '%s>=javascript:gs.daysAgo(%s)' % (daysby, daysAgo) if daysAgo else ''
+
+
+        replaces =[]
+        retrived_objects = {'user':{}, 'group': {}}
+
+        for value_replacement in value_replacements:
+            if '=' in value_replacement:
+                print value_replacement
+                k,v= value_replacement.split('=')
+                replaces.append({'tokey':k, 'fromkey':v})
+
+        if user_names:
+            filterarg = ''
+            params = filterbuilder('user_name', user_names)
+            query_string = '%s/api/now/table/%s?sysparm_query=%s' % (url,'sys_user', params)
+            for record in getRecords(query_string, {'user':username, 'password':password}):
+                retrived_objects['user'][record['sys_id']] = record['user_name']
+                if record['sys_id']:
+                    filterarg = record['sys_id'] if not filterarg else ','.join([filterarg, record['sys_id']])
+            user_sysid = filterbuilder(filterby, filterarg) if filterarg else ''
+
+        if assigment_groups:
+            filterarg = ''
+            params = filterbuilder('name', assigment_groups)
+            query_string = '%s/api/now/table/%s?sysparm_query=%s' % (url,'sys_user_group', params)
+            for record in getRecords(query_string, {'user':username, 'password':password}):
+                retrived_objects['group'][record['sys_id']] = record['name']
+                if record['sys_id']:
+                    filterarg = record['sys_id'] if not filterarg else ','.join([filterarg, record['sys_id']])
+            group_sysid = filterbuilder('assignment_group', filterarg) if filterarg else ''
+        if group_sysid or user_sysid:
+            sysparm_query = [time_range, group_sysid, user_sysid]
+            sysparm_query = [x for x in sysparm_query if x]
+            sysparm_query = '^'.join(sysparm_query)
+            query_string = '%s/api/now/table/%s?sysparm_limit=1000&sysparm_query=%s' % (url, 'incident', sysparm_query)
+            retrived_objects = dict(retrived_objects['user'].items() + retrived_objects['group'].items())
+            for record in getRecords(query_string, {'user': username, 'password':password}):
+                for replace in replaces:
+                    record = keyreplace(record, replace['tokey'], replace['fromkey'], username, password)
+                record['source'] = query_string
+                record['sourcetype'] = 'snow:incident'
+                record['_time'] = time.mktime(datetime.datetime.strptime(record['sys_created_on'], "%Y-%m-%d %H:%M:%S").timetuple())
+                record['opened_at'] = time.mktime(datetime.datetime.strptime(record['sys_created_on'], "%Y-%m-%d %H:%M:%S").timetuple())
+                record['resolved_at'] = time.mktime(datetime.datetime.strptime(record['sys_created_on'], "%Y-%m-%d %H:%M:%S").timetuple())
+                record['closed_at'] = time.mktime(datetime.datetime.strptime(record['sys_created_on'], "%Y-%m-%d %H:%M:%S").timetuple())
+                record['_raw'] = util.tojson(record)
+                yield record
+
+dispatch(getUserCommand, sys.argv, sys.stdin, sys.stdout, __name__)
